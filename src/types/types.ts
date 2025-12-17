a957308a38e86d0240764f5ea3ea239d47d970d0
Ernesto224 <130603932+Ernesto224@users.noreply.github.com>
2025-12-17 14:18:49 -0600

    Modificaciones de estructura
    La version anterior no fue de un agrado total asi que se se remplazo por una plantilla de forma temporal para definir de mejor forma un diseño mas personal , de momento solo queda hacer modificaciones de contenido a este portafolio.

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,86 +1,49 @@
-import { string } from "astro:schema";
-
-export interface AccordionItemsProps {
+export interface SiteConfig extends HeaderProps {
   title: string;
-  text: string;
-  icon: string;
+  description: string;
+  lang: string;
+  author: string;
+  socialLinks: { text: string; href: string }[];
+  socialImage: string;
+  canonicalURL?: string;
 }
 
-export interface IconButtonProps {
-  href?: string;
-  label?: string;
-  ariaLabel?: string;
+export interface SiteContent {
+  hero: HeroProps;
+  experience: ExperienceProps[];
+  projects: ProjectProps[];
+  about: AboutProps;
 }
 
-export interface TechIconProps {
-  icon?: string;
-  label?: string;
-  ariaLabel?: string;
-}
-
-export interface TechCarruselProps {
-  technologies: TechIconProps[];
-}
-
-export interface CardProjectProps {
-  title: string;
-  state?: string;
-  imageSrc?: string;
-  deployHref?: string;
-  gitHref?: string;
-}
-
-export interface ActiveSection {
-  currentSection: string;
-}
-
-export interface SectionDetectorOptions {
-  offset?: number;
-  threshold?: number;
-  onSectionChange?: (sectionId: string) => void;
-}
-
-export interface SectionInfo {
-  id: string;
-  element: HTMLElement;
-  top: number;
-  bottom: number;
-  height: number;
-  isVisible: boolean;
-  visibilityPercentage: number;
-}
-
-export interface InputInfo {
-  id: string;
-  label: string;
-  placeholder?: string;
-  type?: string;
-  value: string;
-  error?: string;
-  required?: boolean;
-  maxLength?: number;
-  validation?: (value: string) => boolean;
-  onChange: (value: string, isValid: boolean) => void;
-}
-
-export interface ImportSchemaEmailJsEnv {
-  readonly EMAIL_JS_API_REST_URL: string;
-  readonly USER_ID: string;
-  readonly ACCESS_TOKEN: string;
-  readonly SERVICE_ID: string;
-  readonly TEMPLATE_ID: string;
-}
-
-export interface EmailTemplateParams {
-  title: string;
+export interface HeroProps {
   name: string;
+  specialty: string;
+  summary: string;
   email: string;
-  message: string;
-  time: string;
 }
 
-export interface ApiResponse<T = any> {
-  success: string;
-  message: string;
-  data?: T
+export interface ExperienceProps {
+  company: string;
+  position: string;
+  startDate: string;
+  endDate: string;
+  summary: string | string[];
+}
+
+export interface ProjectProps {
+  name: string;
+  summary: string;
+  image: string;
+  linkPreview?: string;
+  linkSource?: string;
+}
+
+export interface AboutProps {
+  description: string;
+  image: string;
+}
+
+export interface HeaderProps {
+  siteLogo: string;
+  navLinks: { text: string; href: string }[];
 }
